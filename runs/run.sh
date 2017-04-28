cfcc2401fac7ba6d13330435c592d18d5a569f07
jerryff <fanjiayue0425@163.com>
2017-04-28 08:41:21 +0800

    1

--- runs/run.sh
+++ runs/run.sh
@@ -3,7 +3,7 @@ filelist=`ls /home/vagrant/CRC/traces/`
 for file in $filelist
 do 
 filetail=`echo $file  | cut -d . -f4`
-if ["$filetail"x = "trace"x]
+if [ "$filetail"x = "trace"x ]
 then
 	echo $file
 fi
