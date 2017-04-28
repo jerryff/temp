3bc27d7bf64eac02b8f76d237e6fde2e1d9f8591
jerryff <fanjiayue0425@163.com>
2017-04-28 08:53:51 +0800

    1

--- runs/run.sh
+++ runs/run.sh
@@ -6,6 +6,6 @@ filetail=`echo $file  | cut -d . -f4`
 if [ "$filetail"x = "trace"x ]
 then
 	filename=`echo $file  | cut -d . -f2`
-	../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/${file} -o ${filename}.stats -cache UL3:1024:64:16 -LLCrepl 0 
+	../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/${file} -o ${filename}.stats -cache UL3:1024:64:16 -LLCrepl 0 >output.txt
 fi
 done
