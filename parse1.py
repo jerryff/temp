4781d8ed16615aab797707fd3985900b3dc76ce5
jerryff <fanjiayue0425@163.com>
2017-04-28 10:27:36 +0800

    1

---
+++ parse1.py
@@ -0,0 +1,87 @@
+import os
+result_list = []
+file_list = os.listdir("runs/")
+for filename in file_list:
+    single_list = []
+    one_dict = {"Policy": 'runs/'+filename}
+    f = open('runs/'+filename, 'r')
+    lines = f.readlines()
+    for line in lines:
+        beginer = line.find("CPI:")
+        if beginer != -1:
+            beginer += 5
+            ender = beginer
+            while line[ender].isdigit() or line[ender] == '.':
+                ender += 1
+            cpi = line[beginer:ender]
+            one_dict["CPI"] = cpi
+            continue
+        beginer = line.find("IFETCH    Miss Rate")
+        if beginer != -1:
+            beginer = line.find(":") + 3
+            ender = beginer
+            while line[ender].isdigit() or line[ender] == '.':
+                ender += 1
+            ifetch = line[beginer:ender]
+            one_dict["IFETCH"] = ifetch
+            continue
+        beginer = line.find("LOAD      Miss Rate")
+        if beginer != -1:
+            beginer = line.find(":") + 3
+            ender = beginer
+            while line[ender].isdigit() or line[ender] == '.':
+                ender += 1
+            load = line[beginer:ender]
+            one_dict["LOAD"] = load
+            continue
+        beginer = line.find("STORE     Miss Rate")
+        if beginer != -1:
+            beginer = line.find(":") + 3
+            ender = beginer
+            while line[ender].isdigit() or line[ender] == '.':
+                ender += 1
+            store = line[beginer:ender]
+            one_dict["STORE"] = store
+        beginer = line.find("WRITEBACK Miss Rate")
+        if beginer != -1:
+            beginer = line.find(":") + 3
+            ender = beginer
+            while line[ender].isdigit() or line[ender] == '.':
+                ender += 1
+            write = line[beginer:ender]
+            one_dict["WRITEBACK"] = write
+            continue
+        beginer = line.find("Thread: 0 Lookups")
+        if beginer != -1:
+            beginer = line.rfind(":") + 2
+            ender = beginer
+            while line[ender].isdigit() or line[ender] == '.':
+                ender += 1
+            total = line[beginer:ender]
+            one_dict["TOTAL"] = total
+    single_list.append(one_dict)
+    f.close()
+    result_list.append(single_list)
+
+wf = open("result.txt", 'w')
+
+wf.write("Filename                        CPI         IFETCH      LOAD        STORE       WRITE      TOTAL\n\n")
+for case_list in result_list:
+    wf.write(case_list[0]['Policy'].split('/')[1]   + '\n')
+    for c in case_list:
+        try:
+            line = "%-30s  %-10s  %-10s  %-10s  %-10s  %-10s %-10s\n" % (
+                c['Policy'].split('/')[0] ,
+                c['CPI'],
+                c['IFETCH'],
+                c['LOAD'],
+                c['STORE'],
+                c['WRITEBACK'],
+                c['TOTAL']
+            )
+            wf.write(line)
+        except KeyError as e:
+            print(c['Policy'])
+    wf.write('\n')
+wf.close()
+print("Done!")
--- runs/run.sh
+++ runs/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 filelist=`ls /home/vagrant/CRC/traces/`
+n=0
 for file in $filelist
 do 
 filetail=`echo $file  | cut -d . -f4`
 if [ "$filetail"x = "trace"x ]
 then
 	filename=`echo $file  | cut -d . -f2`
-	../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/${file} -o ${filename}.stats -cache UL3:1024:64:16 -LLCrepl 0 >output.txt
+	../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/${file} -o ${filename}.stats -cache UL3:1024:64:16 -LLCrepl 0 > ../output.txt
+	n=n+1
+	echo $n
 fi
 done
