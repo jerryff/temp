cfcd6b2e80d73c6afcef9071fda272a6e881ffe8
jerryff <fanjiayue0425@163.com>
2017-05-10 18:47:16 +0800

    1

--- runs/run.sh
+++ runs/run.sh
@@ -9,7 +9,7 @@ then
 	filename=`echo $file  | cut -d . -f2`
 	echo $n
 	echo ${filename}
-	time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/${file} -o ../my2/${filename}.stats -cache UL3:1024:64:16 -LLCrepl 2 > ../output.txt
+	time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/${file} -o ../lru/${filename}.stats -cache UL3:1024:64:16 -LLCrepl 0 > ../output.txt
 	n=`expr $n + 1`
 fi
 done
